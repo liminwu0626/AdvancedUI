f91c2bda0da9909af68d96bc708a98c5b9572605
吴立民 <1091423040@qq.com>
2017-05-16 12:51:02 +0800

    2.Paint的实用

---
+++ app/src/main/java/com/whty/advancedui/view/CircleProgressBar.java
@@ -0,0 +1,17 @@
+package com.whty.advancedui.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by wulimin on 2017/5/16.
+ */
+
+public class CircleProgressBar extends View {
+
+    public CircleProgressBar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
